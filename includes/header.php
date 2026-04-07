ada586e68c17ad725322ab20527aecfab34a42ee
Ridham Gadhiya <ridhamgadhiya16@gmail.com>
2026-04-07 18:38:15 +0530

    commit

--- includes/header.php
+++ includes/header.php
@@ -29,8 +29,8 @@ if (isset($_SESSION['role'])) {
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- <link rel="stylesheet" href="../assets/css/style.css"> -->
     <link rel="stylesheet" href="../assets/css/style.css">
+    <!-- <link rel="stylesheet" href="../assets/css/style.css"> -->
 </head>
 
 <body class="bg-light">
